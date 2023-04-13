e344692e49fa120ba1db0d1bb2f2635c5577d240
Vendy <vendy@jedisoft.com>
2023-04-13 16:11:58 +0800

    init

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -3,25 +3,16 @@ import type { APIContext } from 'astro';
 import { getRuntime } from "@astrojs/cloudflare/runtime";
 import type { KVNamespace } from '@cloudflare/workers-types';
 
-export async function get({ request }: APIContext) {
+export async function get({request}: APIContext) {
 
   const runtime = getRuntime(request)
 
-  try {
-    const { MY_KV } = (runtime.env as { MY_KV: KVNamespace });
-    const vv = await MY_KV.get('HELLO')
-    return new Response(JSON.stringify({ param: vv }), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } catch (e) {
-    return new Response(JSON.stringify({ param: 'ERROR' }), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  }
+  const { MY_KV } = (runtime.env as { MY_KV: KVNamespace });
+  // const vv = await MY_KV.get('HELLO')
+  return new Response(JSON.stringify({ param: 'vv' }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 }
