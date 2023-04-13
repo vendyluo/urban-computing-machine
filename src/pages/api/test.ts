f3c95322bd54d4f5bce11d573aed2f5a428fa0d9
Vendy <vendy@jedisoft.com>
2023-04-13 16:29:22 +0800

    init

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -8,8 +8,8 @@ export async function get({request}: APIContext) {
   const runtime = getRuntime(request)
 
   const { MY_KV } = (runtime.env as { MY_KV: KVNamespace });
-  // const vv = await MY_KV.get('HELLO')
-  return new Response(JSON.stringify({ param: 'vv' }), {
+  const vv = await MY_KV.get('HELLO')
+  return new Response(JSON.stringify({ param: vv }), {
     status: 200,
     headers: {
       'Content-Type': 'application/json',
